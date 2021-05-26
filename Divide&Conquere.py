84cda384b5332e65eaf10c6e1505c49bb9a579ae
zulfiqar <zulfikaribraheem@hotmail.com>
2021-05-26 23:51:05 +0200

    Recursive method is added in D & C class

--- Divide&Conquere.py
+++ Divide&Conquere.py
@@ -7,12 +7,17 @@ class Recursive:
     def __init__(self,number):
         print('object crafted')
         self.value_a=number
+        list_numbers=[3,6,8,12,14,17,25,29,31,36,42,47,53,55,62]
+        self.low=0
+        self.high=len(list_numbers)-1
         self.recursive(self.value_a)
+        self.binary_search_recursive_method(self.low,self.high,number)
+
     def recursive(self,value_a):
         """
-        This funtion is written to decrementing numbers from starting numbers
+        [This funtion is written to decrementing numbers from starting numbers
         using recursive method. This methad keep calling itself untill it reaches
-        boundry. 
+        boundry. Print() will be called n times. O(n) is time complexity of this recursive function]
         """
 
         #print(value_a)
@@ -22,6 +27,88 @@ class Recursive:
             self.recursive(value_a-1)
         else:
             return 0
+    def binary_search_iterative_method(self,number):
+        """[This is the iterative method of binary search. First condition of B.S is that list must be in 
+        sorted form. Binary search algorithm is following the D & C strategy ]
+        """
+        list_numbers=[3,6,8,12,14,17,25,29,31,36,42,47,53,55,62]
+        low=0
+        high=len(list_numbers)-1
+        mid=0
+        
+        while low <= high:
 
-re=Recursive(10)
+
+            mid=(low+high)//2
+            value=list_numbers[mid]
+            if value < number:
+                
+                low=mid+1
+
+            elif value > number:
+
+                high=mid-1
+            
+            else:
+                
+                print(' index of number ',mid, 'value ',list_numbers[mid])
+                return mid
+        return -1
+
+    def binary_search_recursive_method(self, low , high, k):
+
+        list_numbers=[3,6,8,12,14,17,25,29,31,36,42,47,53,55,62]
+
+        if low == high:
+
+            if list_numbers[low]==k:
+
+                print('index found at ' , k)
+
+                return k
+
+            else:
+                print('No element found',end=',')
+                return 0
+        else:
+
+            mid = (low + high ) // 2
+
+            if list_numbers[mid] == k:
+
+                print('index found at ' , mid)
+
+                return mid 
+
+            elif k < list_numbers[mid]:
+
+                return self.binary_search_recursive_method(low,mid-1,k)
+
+            elif k > list_numbers[mid]:
+
+                return self.binary_search_recursive_method(mid+1,high,k)
+        
+
+
+
+
+
+
+            
+
+
+
+
+                
+
+
+
+re=Recursive(29)
+index_iterative=re.binary_search_iterative_method(29)
+if index_iterative == -1:
+    print('no value is being found in  terative method', end=',')
+    
+else:
+    print('index of found element in iterative method ',index_iterative , end=',')
+
 
