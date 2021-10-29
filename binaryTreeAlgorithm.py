3faf2b2b54abd7d629a11a9d3ca10ba87848a534
Zulfiqar Ibrahim <zulfikaribraheem@hotmail.com>
2021-10-29 21:47:30 +0200

    new apprach to BT

---
+++ binaryTreeAlgorithm.py
@@ -0,0 +1,31 @@
+
+class node:
+    
+    def __init__(self,data):
+        self.left=None
+        self.right=None
+        self.value=data
+        
+    
+
+a=node('a')
+b=node('b')
+c=node('c')
+d=node('d')
+e=node('e')
+f=node('f')
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+
+        
+    
+
+
+
+        
+    
